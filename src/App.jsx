5c20e070ce005a0fa865382ae069a0e3f03ff520
Matt <84277551+MattRueter@users.noreply.github.com>
2025-10-16 12:41:05 +0200

    reorganize styles into scss file.

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,49 @@ import './App.css'
 
 function App() {
   return (
-    <>
-      <h1>hello</h1>
+    <>    
+      
+      <h1>Matt_Rueter</h1>
+      <h2>Software Developer</h2>
+      <ul>
+        <li>Madrid or remote</li>
+        <li>Legal working status in Spain and U.S.</li>
+      </ul>
+
+      <section>
+        <p>What I can do</p>
+        <p>
+          I’m a software developer who helps individuals and businesses bring their ideas to life on the web.
+        </p>
+        <p>
+          I build modern web applications using technologies like React, TypeScript, and Node.js 
+          — everything from interactive front-ends to full-stack solutions.
+        </p>
+        <p>
+          I’ve worked on production-ready apps in remote Agile teams and can handle the full development process: 
+          from planning and design to launch and maintenance.
+        </p>
+        <p>
+          Based in Madrid and open to remote work. 
+          If you’re looking for a developer who can turn ideas into smooth, user-friendly web experiences, I’d love to chat.
+        </p>
+      </section>
+
+      <section>Proposal process</section>
+
+      <section>Projects and experience
+        <ul>
+          <li>Team Forward</li>
+          <li>Chingu Dashboard</li>
+          <li>Parlanchín</li>
+        </ul>
+      </section>
+
+      <section>
+        <p>contact:</p>
+        <p>get in touch with me through linkedin</p>
+      </section>
+
     </>
   )
 }
