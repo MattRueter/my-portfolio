fd5e66d5d34085ecfe7e2d95fcd04af4ccc5745a
Matt <84277551+MattRueter@users.noreply.github.com>
2025-11-12 16:53:21 +0100

    create draft elt background page.

--- src/LanguageTech.jsx
+++ src/LanguageTech.jsx
@@ -1,8 +1,29 @@
-
+import { Link } from "react-router";
+import './styles/index.scss'
+import './styles/languageTech.scss'
 
 function LanguageTech () {
   return(
-    <h1>hello</h1>
+    <div className="languageTechPage">
+      <Link to="/">go back</Link>
+      <section className="textSection">
+        <h1>ELT background</h1>
+        <p>Before software development I worked for several years in the foreign language teaching industry. 
+          I had several roles within International House Madrid including teacher, senior teacher and ADOS. As a software developer I 
+          can work within any domain but given my background I especially enjoy working on language learning related applications.
+        </p>
+        <p>Additionally, because of my background in teaching languages I am well placed to understand the needs
+          and wishes of teachers, students, and support staff within a language academy. 
+        </p>
+        <p>Some ideas if you don't already have some of your own...</p>
+        <ul>
+          <li>automate placement testing</li>
+          <li>material creation based on syllabus & course materials</li>
+          <li>student app to reinforce content from lessons</li>
+        </ul>
+      </section>
+    </div>
+
   )
 };
 
